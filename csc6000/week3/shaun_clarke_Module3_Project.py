95b3dcefd476e9d1cae3282b16aded51b4f7237b
Shaun <shaunclarke43@gmail.com>
2025-04-04 17:42:40 -0700

    completed week 3 project

--- csc6000/week3/shaun_clarke_Module3_Project.py
+++ csc6000/week3/shaun_clarke_Module3_Project.py
@@ -30,9 +30,10 @@ This GP sum with 10 elements is equal to 1023
 the first elements are 1, 2, and 4
 """
 
+# Function to get the first three elements
 def get_frist_three_terms(a,r):
     """
-    # iterating through the formula to find the first three terms and adding each term to the list
+    iterating through the formula to find the first three terms and adding each term to the list
     """
     three_terms_list = []
      # iterating through the formula to find the first three terms and adding each term to the list
@@ -42,9 +43,77 @@ def get_frist_three_terms(a,r):
 
     return three_terms_list
 
+# Function to ask for user input
+def get_input():
 
+    """
+    Ask the user to enter a scale factor or common ratio
+    """
+
+    # Variables
+    a=""
+    r= ""
+
+    # While loop to ensure that the user enters scalar number as input.
+    while type(a) != int or type(a) != float:
+
+        try:
+            get_input = input(f"Enter a scale factor(a) you must, a number it should be: ")
+            print("")
+
+            if get_input == "exit":
+                exit()
+
+            a = float(get_input) 
+            if a.is_integer():
+                a = int(a)
+
+            if type(a) == int or type(a) == float:
+                break
+
+        except ValueError:
+            print(f"Lets try this again Young Jedi...")
+            print("")
+
+    # While loop to ensure that the user enters common ratio number as input.
+    while type(r) != int or type(r) != float:
+
+        try:
+            get_input = input(f"Enter a common ratio(r) you must, a number it should be: ")
+            print("")
+
+            if get_input == "exit":
+                exit()
+
+            r = float(get_input)
+            if r.is_integer():
+                r = int(r)
+
+            if type(r) == int or type(r) == float:
+                break
+            
+        except ValueError:
+            print(f"Lets try this again Young Jedi...")
+            print("")
+
+    return a,r
+
+# Function to get the specific sum of a geometric progression
 def sum_gp(a, r):
+    """
+    **Instructions**
+    Write a Python program that:
 
+    ○ Asks the user a scale factor a and a common ratio r;
+
+    ○ Your program inform the user if the GP informed converges to a sum regardless of its number of elements:
+
+    ■ If the GP converges to a sum regardless of the number of elements, your program should compute the sum of infinite elements;
+
+    ■ Otherwise, your program should ask the number of elements n, and compute the sum of all elements;
+
+    ○ With that information, your program should print out the first 3 elements of the GP and the computed sum.
+    """
     if r == -1:
         print(f"Minus 1")
         print("This GP does not converge to a finite number with infinite elements")
@@ -67,9 +136,6 @@ def sum_gp(a, r):
         print(f"This GP sum with {nth_term} elements is equal to {negative_one_total}")
         print(f"The first terms are {frist_three_terms[0]}, {frist_three_terms[1]}, and {frist_three_terms[2]}")
 
-        
-  
-
     elif r == 1:
         print(f"Equals 1")
         print("This GP does not converge to a finite number with infinite elements")
@@ -85,11 +151,10 @@ def sum_gp(a, r):
 
         print(f"This GP sum with {nth_term} elements is equal to {sum}")
         print(f"The first terms are {frist_three_terms[0]}, {frist_three_terms[1]}, and {frist_three_terms[2]}")
-        
 
     elif r < 1:
         # Calculating the infinite sum
-        infinite_sum = (a /(1-r))
+        infinite_sum = round((a /(1-r)))
 
         # Calling function to to find the first three terms
         frist_three_terms = get_frist_three_terms(a,r)
@@ -110,14 +175,17 @@ def sum_gp(a, r):
         # Calling function to to find the first three terms
         frist_three_terms = get_frist_three_terms(a,r)
 
-        print(f"This GP sum with {nth_term} elements is equal to {finite_sum}")
+        print(f"This GP sum with {nth_term} elements is equal to {round(finite_sum)}")
         print(f"The first terms are {frist_three_terms[0]}, {frist_three_terms[1]}, and {frist_three_terms[2]}")
 
-sum_gp(-2, -1)
+# Getting user input
+a,r = get_input()
+
+sum_gp(a,r)
 
 # Need to do:
+
 """
-Allow user to enter scalar (a) and ratio (r) as input
-Add while loops to make sure the user enters the correct item as int and not string.
+use is_integer to handle outputs that should not be a float.
 
 """
