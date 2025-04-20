69d31e450c2021b777f702d698c4aed6657bce20
Shaun <shaunclarke43@gmail.com>
2025-04-19 19:54:25 -0700

    added the requirements for week 5 tasks

---
+++ csc6000/week5/shaun_clarke_Module5_Project.py
@@ -0,0 +1,21 @@
+"""
+Instructions
+Write a Python program to print Pascal's Triangle.
+
+Your program should ask the user the number of lines (use a limit from 4 to 8 lines) of the Pascal's Triangle;
+Your program should have a function that computes the binomial coefficient of n and k, and this function should be used to find the values of the cells of the Pascal's Triangle;
+The great challenge of this program is the output formatting which requires you to assemble the lines with the proper space.
+"""
+
+#Psuedo coding it
+"""
+Ask for input
+So whatever limit the user enters, we will loop through that limit plus 1 so we get the number of rows for the triangle.
+We do plus 1 because index starts at 0 in the loop and stops just before the actual number we are looping through.
+So looping through 4 + 1 will give us 0,1,2,3,4. The rows of the triangle from 0 to 4.
+Looking at pascals triangle while using the formula (n,k) with n at the top of the parenthesis and k at the bottom.
+You see that k in each cell of the triangle is the range of the row number plus 1. so if it is row 3, k in each of the three cells would be 0,1,2,3.
+Keep in mind the triangle starts from row 0.
+
+
+"""
