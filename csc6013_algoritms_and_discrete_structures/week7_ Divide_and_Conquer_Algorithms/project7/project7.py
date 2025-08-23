6e368c47ff572f3d571a7728e7defeaa23b56952
Shaun <shaunclarke43@gmail.com>
2025-08-22 17:09:27 -0700

    working code

---
+++ csc6013_algoritms_and_discrete_structures/week7_ Divide_and_Conquer_Algorithms/project7/project7.py
@@ -0,0 +1,17 @@
+"""
+Develop one Python program to perform the Quicksort, but instead of sorting the elements in ascending order (from the smallest to the largest),
+the elements are sorted in the decrescent order (from the larger to the smallest).
+
+Your program also have to compute and print at the end the number of swaps performed and the number of recursive calls.
+
+Test your program over the following arrays:
+
+A = [38, 21, 39, 60, -1, 10, 81, 23]
+B = [2, 97, 5, 88, 9, 72, 12, 64, 17, 56, 21]
+C = [100, 33, 22, 213, 65, 29, 153, 199, 47, 181, 85]
+
+You have to submit the code (.py file) of your algorithm, plus the .pdf of the trace for the three required tests.
+It is expected that your code performs the computations necessary for the traces (count the number of calls and swaps).
+
+
+"""
