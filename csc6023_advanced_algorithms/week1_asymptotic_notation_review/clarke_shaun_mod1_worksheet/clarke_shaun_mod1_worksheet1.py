779ccc4522c14b61f5fcde6b711b0faa75f1549a
Shaun <shaunclarke43@gmail.com>
2025-09-04 16:17:11 -0700

    working draft, need to make oop

--- csc6023_advanced_algorithms/week1_asymptotic_notation_review/clarke_shaun_mod1_worksheet/clarke_shaun_mod1_worksheet1.py
+++ csc6023_advanced_algorithms/week1_asymptotic_notation_review/clarke_shaun_mod1_worksheet/clarke_shaun_mod1_worksheet1.py
@@ -8,3 +8,45 @@ Task:
     Prints out the maximum value
 """
 
+from typing import List
+import random
+
+
+# Maximum Contiguous Subsequence Sum Function
+def MCSS(a: List[int]) -> int:
+    """
+    """
+    # declaring variables for counter, largest sum and i.
+    largest, acc, i = 0, 0, 0
+    # looping the array to find the largest sub sequence
+    for j in range(len(a)):
+        print(f"This is J: {j}")
+        # Incrementing acc by the next item in the list.
+        acc += a[j]
+        print(f"This is acc: {acc}\n")
+        # If the number or numbers summed so far is greater than largest, update largest with acc.
+        if (acc > largest):
+            largest = acc
+            print(f"This is largest: {largest}\n")
+        elif (acc < 0): # if acc is negative reset it to 0.
+            i = j + 1
+            acc = 0
+    return largest
+
+
+# a = [2,3, -4, -5,5,6,7]
+# a = [5, -1, 56, -3, -18, 22, -9]
+
+# empty list to hold generated numbers
+a = []
+# declaring start and stop variables for the random number range
+start, stop = -10, 10
+# looping through and generating 1000 ints
+for i in range(stop):
+    a.append(random.randint(start, stop))
+
+print(a)
+
+print(f"{MCSS(a)}")
+
+
