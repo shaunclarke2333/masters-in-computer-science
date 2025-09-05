29c590cd4fed4d87cef9dbe968fb92ea0a2c5043
Shaun <shaunclarke43@gmail.com>
2025-09-04 20:58:45 -0700

    completed task 1

--- csc6023_advanced_algorithms/week1_asymptotic_notation_review/clarke_shaun_mod1_worksheet/clarke_shaun_mod1_worksheet2.py
+++ csc6023_advanced_algorithms/week1_asymptotic_notation_review/clarke_shaun_mod1_worksheet/clarke_shaun_mod1_worksheet2.py
@@ -8,16 +8,6 @@ Task:
     Go to IDLE and try to program it
 """
 
-"""
-Author: Shaun Clarke
-Class: CSC6023 Advanced Algorithms
-Module: Module 1 Asymptotic Notation Review
-Task:
-    Worksheet Task 01
-    Create a program that randomizes a vector of 1000 positive and negative integers, then finds the maximum contiguous subsequence sum value
-    Prints out the maximum value
-"""
-
 from typing import List
 import random
 
