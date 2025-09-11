f68f358a06fadb9befc4b83864cac347d58df483
Shaun <shaunclarke43@gmail.com>
2025-09-10 19:47:35 -0700

    setup week 2 project

--- csc6023_advanced_algorithms/week2_dynamic_programming/clarke_shaun_mod2_pa/clarke_shaun_mod2_pa1.py
+++ csc6023_advanced_algorithms/week2_dynamic_programming/clarke_shaun_mod2_pa/clarke_shaun_mod2_pa1.py
@@ -4,5 +4,22 @@ Class: CSC6023 Advanced Algorithms
 Module: Module 2 Dynamic Programming
 Project Assignment:
     Instructions
-    
+    Project #2
+
+        Create a program that computes the "Tribonacci" sequence numbers
+        ○Unlike the traditional Fibonacci sequence (a number is the sum of the two previous ones), here a number is the sum of the three previous ones (the initial numbers are 1,1,1)
+
+        The first 9 elements of the sequence are:
+        1, 1, 1, 3, 5, 9, 17, 31, 57, …
+
+        ■Your program should asks the user a positive Integer n and the deliver the n-th element of the Tribonacci sequence
+
+        For example, for n = 6, it delivers 9
+        Make sure your program uses Dynamic Programming in an efficient way (for example, keeping in memory previously computed elements)
+        This program must be your own, do not use someone else’s code
+        Any specific questions about it, please bring to the Office hours meeting this Monday or contact me by email
+        This is a challenging program to make sure you are mastering your Python programming skills, as well as your asymptotic analysis understanding
+        Don’t be shy with your questions
+        The program should not crash if the user enters something other than an integer (like a real number or a non-numeric value) 
+        The program should end when the user enters an integer less than 1.
 """
