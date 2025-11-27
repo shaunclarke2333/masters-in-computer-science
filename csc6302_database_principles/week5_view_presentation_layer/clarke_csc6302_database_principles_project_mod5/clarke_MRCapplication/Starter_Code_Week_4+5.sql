94b8be1eccecbb17e751a979cab5d87c35cf10cb
Shaun <shaunclarke43@gmail.com>
2025-11-26 17:58:13 -0800

    started building out pages

---
+++ csc6302_database_principles/week5_view_presentation_layer/clarke_csc6302_database_principles_project_mod5/clarke_MRCapplication/Starter_Code_Week_4+5.sql
@@ -0,0 +1,340 @@
+/* WEEK 4 & 5 STARTER CODE */
+
+DROP DATABASE IF EXISTS `mrc`;
+CREATE DATABASE IF NOT EXISTS `mrc`; 
+USE `mrc`;
+
+DROP TABLE IF EXISTS `vessels`;
+
+CREATE TABLE `vessels` (
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `Vessel` varchar(50) NOT NULL,
+  `Cost_Per_Hour` decimal(6,2) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+);
+
+INSERT INTO `vessels` VALUES 
+	(1,'Ocean Voyager',200.00),
+	(2,'Sea Breeze',100.00),
+    (3,'The Warrior',150.00);
+
+
+DROP TABLE IF EXISTS `passengers`;
+
+CREATE TABLE `passengers` (
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `First_Name` varchar(50) NOT NULL,
+  `Last_Name` varchar(50) NOT NULL,
+  `Street` varchar(50) DEFAULT NULL,
+  `City` varchar(50) DEFAULT NULL,
+  `State` CHAR(2) DEFAULT NULL,
+  ZIP CHAR(5) DEFAULT NULL,
+  `phone` CHAR(12) DEFAULT NULL,
+  `getsSeasick` tinyint DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+);
+
+INSERT INTO `passengers` VALUES 
+	(1,'Emily', 'Clark','456 Pine St', 'Rivertown', 'MA', '23456','978-555-5678',NULL),
+	(2,'Michael', 'Lee','789 Maple Ave', 'Beachside', 'MA', '34567','978-555-8765',NULL),
+    (3,'Jessica', 'Adams','654 Birch Rd', 'Seaside', 'MA', '56789','978-555-8760',NULL),
+    (4,'Sarah', 'Johnson','321 Elm St', 'Townsville', 'MA', '45678','978-555-4321',NULL),
+    (5,'John', 'Smith','123 Oak St', 'Cityville', 'MA', '01234','413-555-1234',NULL);
+
+
+DROP TABLE IF EXISTS `trips`;
+
+CREATE TABLE `trips` (
+  `Vessel_ID` int NOT NULL,
+  `Passenger_ID` int NOT NULL,
+  `Date` date NOT NULL,
+  `Departure_Time` time NOT NULL,
+  `Length_in_Hours` decimal(5,2) NOT NULL,
+  `Total_Passengers` int NOT NULL,
+  PRIMARY KEY (`Vessel_ID`,`Date`,`Departure_Time`),
+  FOREIGN KEY (`Vessel_ID`) REFERENCES `vessels` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (`Passenger_ID`) REFERENCES `passengers` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+INSERT INTO `trips` VALUES 
+	(1,1,'2025-03-01', '09:00:00',3.00,3),
+	(1,1,'2025-03-02', '10:00:00',2.00,3),
+    (1,1,'2025-03-05', '11:30:00',3.50,3),
+    (1,1,'2025-03-09', '09:30:00',1.50,3),
+    (1,2,'2025-03-03', '11:00:00',4.00,6),
+    (1,2,'2025-03-04', '09:30:00',2.00,6),
+    (1,2,'2025-03-10', '10:30:00',3.00,6),
+    (1,2,'2025-03-12', '08:45:00',3.50,6),
+    (1,2,'2025-03-14', '07:00:00',3.00,6),
+    (2,3,'2025-03-06', '07:00:00',2.00,2),
+    (2,3,'2025-03-10', '08:00:00',2.00,2),
+    (2,3,'2025-03-11', '09:30:00',2.00,2),
+    (2,4,'2025-03-03', '12:30:00',2.50,4),
+    (2,4,'2025-03-04', '07:45:00',3.00,4),
+    (2,4,'2025-03-09', '07:00:00',3.00,4),
+    (2,4,'2025-03-15', '11:30:00',3.50,4),
+    (3,5,'2025-03-02', '08:30:00',1.50,5),
+    (3,5,'2025-03-10', '12:00:00',2.50,5);
+
+SET SQL_SAFE_UPDATES = 0;
+
+/* Views */
+
+CREATE VIEW `all trips` AS
+	SELECT 
+    concat(DATE_FORMAT(t.date,'%m/%d/%Y'), ' @ ', TIME_FORMAT(t.Departure_Time,'%h:%i %p')) AS `Date and Time`,
+    v.Vessel as `Vessel Name`,
+    concat(p.first_name, ' ', p.last_name) AS `Passenger Name`,
+    concat(p.street, ', ', p.city, ', ', p.state, ' ', p.ZIP) AS `Passenger Address`,
+    p.Phone AS `Passenger Phone`,
+    t.Length_in_hours AS `Trip Duration`,
+    CONCAT('$',FORMAT(t.Length_in_hours * v.Cost_Per_Hour,2)) AS `Total Cost`
+    FROM trips t
+    LEFT JOIN vessels v on v.ID = t.Vessel_ID
+    LEFT JOIN passengers p on p.ID = t.Passenger_ID
+    ORDER BY t.date DESC, t.departure_time DESC;
+    
+-- SELECT * FROM `all trips`;
+
+CREATE VIEW `total revenue By vessel` AS
+SELECT `Vessel Name`,
+		CONCAT('$',FORMAT(SUM(CAST(SUBSTRING(`Total Cost`,2) AS DECIMAL(10,2))),2)) AS Revenue
+FROM `all trips`
+GROUP BY `Vessel Name`;
+
+-- SELECT * FROM `total revenue by vessel`;
+
+/* Functions */
+
+DROP FUNCTION IF EXISTS getVesselID;    
+Delimiter $$
+
+CREATE FUNCTION getVesselID(myVesselName VARCHAR(50))
+RETURNS INT DETERMINISTIC
+BEGIN
+	DECLARE foundVesselID INT;
+    SELECT ID INTO foundVesselID 
+    FROM vessels
+    WHERE Vessel = myVesselName;
+	
+    IF foundVesselID is null
+    THEN SET foundVesselID = -1;
+    END IF;
+
+    RETURN foundVesselID;
+END$$
+
+Delimiter ; 
+
+
+DROP FUNCTION IF EXISTS getPassengerID;    
+Delimiter $$
+
+CREATE FUNCTION getPassengerID(myPassengerFName VARCHAR(50), myPassengerLName VARCHAR(50))
+RETURNS INT DETERMINISTIC
+BEGIN
+	DECLARE foundPassengerID INT;
+    SELECT ID INTO foundPassengerID 
+    FROM passengers
+    WHERE First_Name = myPassengerFName
+    AND Last_Name = myPassengerLName;
+	
+    IF foundPassengerID is null
+    THEN SET foundPassengerID = -1;
+    END IF;
+
+    RETURN foundPassengerID;
+END$$
+
+Delimiter ;
+
+-- SELECT getVesselID('Sea Breeze');
+-- SELECT getVesselID('Sea Bream');
+
+-- SELECT getPassengerID('John', 'Smith');
+-- SELECT getPassengerID('Jim', 'Smith');
+
+/* Procedures */
+	
+    /* Gets */
+
+DROP PROCEDURE IF EXISTS getPassengerList;
+DROP PROCEDURE IF EXISTS getVesselList;
+DROP PROCEDURE IF EXISTS getTripList;
+
+
+DELIMITER $$
+
+CREATE PROCEDURE getPassengerList()
+BEGIN
+	SELECT * FROM passengers;
+END$$
+
+CREATE PROCEDURE getVesselList()
+BEGIN
+	SELECT * FROM vessels;
+END$$
+
+CREATE PROCEDURE getTripList()
+BEGIN
+	SELECT * FROM `All Trips`;
+END$$
+
+Delimiter ;
+
+-- CALL getPassengerList();
+-- CALL getVesselList();
+-- CALL getTripList();
+	
+    /* Additions */
+
+DROP PROCEDURE IF EXISTS addPassenger;
+
+DELIMITER $$
+
+CREATE PROCEDURE addPassenger(IN passFName VARCHAR(50), passLName VARCHAR(50), passPhone VARCHAR(50))
+
+BEGIN
+	DECLARE foundPassengerID INT;
+    
+	SELECT getPassengerID(passFName, passLName) INTO foundPassengerID;
+    
+    IF foundPassengerID = -1
+    THEN INSERT INTO passengers (First_Name, Last_name, phone)
+    VALUES (passFName, passLName, passPhone);
+    END IF;
+    
+    SELECT getPassengerID(passFName, passLName) AS PassengerID;
+    
+END$$
+
+DELIMITER ;
+
+
+DROP PROCEDURE IF EXISTS addVessel;
+
+DELIMITER $$
+
+CREATE PROCEDURE addVessel(IN myVesselName VARCHAR(50), myVesselCPH DECIMAL(6,2))
+
+BEGIN
+	DECLARE foundVesselID INT;
+    
+	SELECT getVesselID(myVesselName) INTO foundVesselID;
+    
+    IF foundVesselID = -1
+    THEN INSERT INTO vessels (vessel, Cost_Per_Hour)
+    VALUES (myVesselName, myVesselCPH);
+    END IF;
+    
+    SELECT getVesselID(myVesselName) AS VesselID;
+    
+END$$
+
+DELIMITER ;
+
+
+DROP PROCEDURE IF EXISTS addTrip;
+
+DELIMITER $$
+
+CREATE PROCEDURE addTrip(myVesselName VARCHAR(50), myPassengerFName VARCHAR(50), myPassengerLName VARCHAR(50), myDate DATE, myTime TIME,
+						 myLengthofTrip decimal(5,2), myTotalPassengers INT)
+
+BEGIN
+	DECLARE foundVesselID INT;
+    DECLARE foundPassengerID INT;
+    DECLARE missingData INT;
+    DECLARE duplicate INT;
+        
+	SELECT GETVESSELID(myVesselName) INTO foundVesselID;
+	SELECT GETPASSENGERID(myPassengerFName, myPassengerLName) INTO foundPassengerID;
+    
+	SELECT count(*)
+    FROM trips
+    WHERE Vessel_ID = foundVesselID
+    AND Passenger_ID = foundPassengerID
+    AND Date = myDate
+    AND Departure_Time = myTime
+    INTO duplicate;
+    
+    IF duplicate > 0
+		THEN SELECT 0 as DuplicateTrip;
+    ELSEIF foundVesselID = -1 AND foundPassengerID = -1
+		THEN SELECT -3 AS NotFound;
+    ELSEIF foundPassengerID = -1
+		THEN SELECT -2 AS NotFound;
+    ELSEIF foundVesselID = -1
+		THEN SELECT -1 AS NotFound;
+    ELSE INSERT INTO trips
+		VALUES(foundVesselID, foundPassengerID, myDate, myTime, myLengthofTrip, myTotalPassengers);
+	END IF;
+    
+END;$$
+
+DELIMITER ;
+
+
+-- CALL addPassenger('John','Smith', '123-456-1900');
+-- CALL addPassenger('Jim','Smith', '123-456-1900');
+-- SELECT * FROM Passengers;
+
+-- CALL addVessel('Sea Breeze', 100);
+-- CALL addVessel('Sea Bream', 250);
+-- SELECT * FROM Vessels;
+
+-- CALL addTrip('Sea Breeze', 'John', 'Smith', '2025-06-30', '12:00:00', 3, 5);
+-- CALL addTrip('Sea Bread', 'John', 'Smith', '2025-06-30', '12:00:00', 3, 5);
+-- CALL addTrip('Sea Breeze', 'Jen', 'Smith', '2025-06-30', '12:00:00', 3, 5);
+
+	/* Deletions */ 
+    
+DROP PROCEDURE IF EXISTS deletePassenger;
+
+DELIMITER $$
+
+CREATE PROCEDURE deletePassenger(IN passID INT)
+
+BEGIN
+	DECLARE foundPassengerID INT;
+    
+    SELECT count(ID) FROM passengers WHERE ID = passID INTO foundPassengerID;
+    
+    IF foundPassengerID > 0
+    THEN DELETE FROM Passengers WHERE ID = passID;
+    ELSE SELECT -1 AS NotFound;
+    END IF;
+    
+    
+END$$
+
+DELIMITER ;
+
+-- CALL deletePassenger(1);
+-- SELECT * FROM Passengers;
+
+
+DROP PROCEDURE IF EXISTS deleteVessel;
+
+DELIMITER $$
+
+CREATE PROCEDURE deleteVessel(IN vesselID INT)
+
+BEGIN
+	DECLARE foundVesselID INT;
+    
+    SELECT count(ID) FROM Vessels WHERE ID = vesselID INTO foundVesselID;
+    
+    IF foundVesselID > 0
+    THEN DELETE FROM vessels WHERE ID = vesselID;
+    ELSE SELECT -1 AS NotFound;
+    END IF;
+    
+    
+END$$
+
+DELIMITER ;
+
+-- CALL deleteVessel(1);
+-- SELECT * FROM Vessels;
+-- CALL getTripList();
--- csc6302_database_principles/week5_view_presentation_layer/clarke_csc6302_database_principles_project_mod5/clarke_MRCapplication/app.py
+++ csc6302_database_principles/week5_view_presentation_layer/clarke_csc6302_database_principles_project_mod5/clarke_MRCapplication/app.py
@@ -1,9 +1,5 @@
 from flask import Flask
-from flask import render_template
-from flask import request
-from flask import redirect
-from flask import url_for
-from flask import flash
+from flask import render_template, request, redirect, url_for, flash, session
 from typing import List, Tuple
 import pandas as pd
 import mysql.connector
@@ -33,6 +29,7 @@ def make_dataframe(rows: List, column_names: List) -> str:
     df = pd.DataFrame(rows, columns=column_names)
     print(df.to_string(index=False, justify='center'))
 
+
 # Creating config parser object to access database configurations.
 config = configparser.ConfigParser()
 
@@ -58,8 +55,14 @@ vessel_service = bll.VesselService(dal.VesselsDal(db_actions))
 passenger_service = bll.PassengerService(dal.PassengersDal(db_actions))
 trip_service = bll.TripService(dal.TripsDal(db_actions))
 
+# rows, column_names = trip_service.get_view_all_trips()
+# print(make_dataframe(rows, column_names))
+
 # Creating the web app
 app = Flask(__name__)
+
+# Creating dummy secret key for login sessions
+app.secret_key = "just-a-dummy-key"
 
 
 # Creating homepage route
@@ -67,11 +70,6 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
-# Creating view all trips route
-@app.route("/all-trips-view")
-def all_trips_view():
-    rows, column_names = trip_service.get_view_all_trips()
-    return render_template("index.html", rows=rows, column_names=column_names)
 
 # Creating login page route
 @app.route("/login", methods=["GET", "POST"])
@@ -86,17 +84,163 @@ def login():
         if not username or not password:
             error = "Username and password are required."
         else:
-            # TODO: replace this with a real BLL call
-            # e.g. user = user_service.authenticate(username, password)
+            # validate the users credentials
             if username == "admin" and password == "secret":
-                # flash("Logged in successfully!")
-                return redirect(url_for("all_trips_view"))
+                # Saving authenticated username in session to use throught the session
+                session["username"] = username
+                
+                username = session.get("username")
+                # Confimring login to user and starting session with flasj
+                flash("Logged in successfully!")
+                # Converting first letter in name to uppercase
+                username = username.capitalize()
+                # redirecting logged in user to their dashboard
+                return redirect(url_for("dashboard", username=username))
             else:
                 error = "Invalid username or password."
 
     # GET or failed POST: re-render form
     return render_template("login.html", error=error)
 
+
+# Creating dashboard route
+@app.route("/dashboard")
+def dashboard():
+    if "username" not in session:
+        flash("Please log in first.")
+        return redirect(url_for("login"))
+    
+    return render_template("dashboard.html")
+
+
+# Creating add passenger route with variables needed to add passenger to DB
+@app.route("/add-passenger", methods=["GET", "POST"])
+def add_passenger():
+
+    # Setting validation error place holder if anything form related fails
+    error = None
+
+    if request.method == "POST":
+        """
+        Stripping whitespace from input.
+        using empty quotes to ensure app does not crash
+        """  
+        first_name: str = request.form.get("first_name", "").strip()
+        last_name: str = request.form.get("last_name", "").strip()
+        phone: str = request.fomr.get("phone", "").strip()
+
+        if not first_name or not last_name or not phone:
+            error = "First name, Last name and Phone are required"
+        else:
+            try:
+                rows, column_names = passenger_service.add_passenger(first_name, last_name, phone)
+                if rows is not None:
+                    added_user_id: int = rows[0][0]
+                    if isinstance(added_user_id, int):
+                        # Confimring Passenger was added and starting session with flasj
+                        flash("Passenger added successfully!")
+                        #redirecting user back to their dashboard with their username to display
+                        username = session.get("username")
+                        return redirect(url_for("dashboard", username=username))
+                    
+                    raise ValueError(f"Passenger was not added")
+                else:
+                    raise ValueError(f"Passenger was not added")
+                    
+            except Exception as err:
+                error = err
+                return render_template("add_passenger.html", error=error)
+
+    
+
+ 
+# Creating add vessel route
+@app.route("/add-vessel", methods=["GET", "POST"])
+def add_vessel():
+    
+    # Setting validation error place holder if anything form related fails
+    error = None
+
+    # Logic to run if the users submits the form
+    if request.method == "POST":
+        """
+        Stripping whitespace from input.
+        using empty quotes to ensure app does not crash
+        """  
+        vessel_name: str = request.form.get("vessel_name", "").strip()
+        cost_per_hr: int = request.form.get("cost_per_hr", "").strip()
+
+        if not vessel_name or not cost_per_hr:
+            error = "Vessel name and cost per hr are required"
+        else:
+            try:
+                rows, column_names = vessel_service.add_vessel(vessel_name, cost_per_hr)
+                if rows is not None:
+                    added_vessel_id: int = rows[0][0]
+                    if isinstance(added_vessel_id, int):
+                        # Confimring vessel was added and starting session with flash
+                        flash("Vessel added successfully!")
+                        #redirecting user back to their dashboard with their username to display
+                        username = session.get("username")
+                        return redirect(url_for("dashboard", username=username))
+                    
+                    raise ValueError(f"Vessel was not added")
+                else:
+                    raise ValueError(f"Vessel was not added")
+                    
+            except Exception as err:
+                error = err
+                return render_template("add_vessel.html", error=error)
+
+# # Creating add trip route
+# @app.route("/add-trip", ["GET", "POST"])
+# # Creating view all trips route
+# def add_trip():
+
+#     # Setting validation error place holder if anything form related fails
+#     error = None
+
+#     # Logic to run if the users submits the form
+#     if request.method == "POST":
+#         """
+#         Stripping whitespace from input.
+#         using empty quotes to ensure app does not crash
+#         """  
+#         vessel_name: str = request.form.get("vessel_name", "").strip()
+#         first_name: str = request.form.get("first_name", "").strip()
+#         last_name: str = request.form.get("last_name", "").strip()
+#         date: str = request.form.get("date", "").strip()
+#         time: str = request.form.get("time", "").strip()
+#         trip_length: str = request.form.get("trip_length", "").strip()
+#         total_passengers: str = request.form.get("total_passengers", "").strip()
+
+#         if not vessel_name or not cost_per_hr:
+#             error = "Vessel name and cost per hr are required"
+#         else:
+#             try:
+#                 rows, column_names = vessel_service.add_vessel(vessel_name, cost_per_hr)
+#                 if rows is not None:
+#                     added_vessel_id: int = rows[0][0]
+#                     if isinstance(added_vessel_id, int):
+#                         # Confimring vessel was added and starting session with flash
+#                         flash("Vessel added successfully!")
+#                         #redirecting user back to their dashboard with their username to display
+#                         username = session.get("username")
+#                         return redirect(url_for("dashboard", username=username))
+                    
+#                     raise ValueError(f"Vessel was not added")
+#                 else:
+#                     raise ValueError(f"Vessel was not added")
+                    
+#             except Exception as err:
+#                 error = err
+#                 return render_template("add_vessel.html", error=error)
+
+
+@app.route("/all-trips-view")
+def all_trips_view():
+    rows, column_names = trip_service.get_view_all_trips()
+    return render_template("view_all_trips.html", rows=rows, column_names=column_names)
 
 
 @app.route("/test/<name>")
